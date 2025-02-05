4dec5210ca983ff067fb676a09c56c7157a69b26
Avanishyadav9035 <132216852+Avanishyadav9035@users.noreply.github.com>
2025-02-05 12:22:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function stringChop(str, size) {
   // your code here
-   size = parseInt(size, 10); // Convert size to a number
-
-  if (isNaN(size) || size <= 0) return []; // Handle invalid input
-
+  if (!str) return [];
+  
   let chunks = [];
   for (let i = 0; i < str.length; i += size) {
     chunks.push(str.substring(i, i + size)); // Extract chunk of 'size' characters
@@ -12,6 +10,6 @@ function stringChop(str, size) {
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size."); 
+const str = prompt("Enter String."); 
+const size = prompt("Enter Chunk Size."); x
 alert(stringChop(str, size)); 
