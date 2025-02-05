f35a897787591a1aab178a6779e10005cf2a78e7
Avanishyadav9035 <132216852+Avanishyadav9035@users.noreply.github.com>
2025-02-05 12:04:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	let words = str.split(" ");
+    let chunks = [];
+    let currentChunk = [];
+
+    for (let word of words) {
+        if (currentChunk.join(" ").length + word.length + 1 <= size) {
+            currentChunk.push(word);
+        } else {
+            chunks.push(currentChunk.join(" "));
+            currentChunk = [word];
+        }
+    }
+    if (currentChunk.length) chunks.push(currentChunk.join(" "));
+
+    return chunks;
 }
 
 // Do not change the code below
